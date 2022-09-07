9e2395f8d56d02f741f8d4496653c326468f0127
tanferest <tanferest@gmail.com>
2022-09-07 12:52:26 +0200

    basic folder structure

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import '../styles/App.css';
+// Fichero src/components/App.js
+
+import {Route, Routes} from 'react-router-dom';
+
+const App = () => {
+  return <div>Hola mundo</div>;
+};
+
+export default App;
